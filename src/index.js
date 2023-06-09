fa6b10d9b98dea4cb19429190559195cb95c6a70
xuxuan <xuxuan@itouchtv.cn>
2023-06-09 18:15:53 +0800

    初始化项目的结构

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { Suspense } from 'react';
+
 import App from '@/App';
+import 'normalize.css';
 import '@/assets/css/index.less';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={'loading'}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Suspense>
   </React.StrictMode>
 );
